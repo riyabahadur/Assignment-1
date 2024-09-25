be7e81ee58e2a7db56c2b27605d106eda72db140
riyabahadur <riya.bahadur_ug2023@ashoka.edu.in>
2024-09-25 20:25:35 +0530

    Update a1-4.cpp

--- a1-4.cpp
+++ a1-4.cpp
@@ -5,11 +5,42 @@ using namespace std;
 //write your code here
 
 // Function to find the median
-double findMedian(int arr1[], int size1, int arr2[], int size2) {
-    
-// Your code here
+double findMedian(int arr1[], int size1, int arr2[], int size2)
+{
+    int i = 0, j = 0; // pointers for arr1 and arr2
+    int merged_size = size1 + size2;
+    int merged[merged_size]; // to store merged array
+    int k = 0;
 
 }
+
+//merging arrays
+
+while (i < size1 && j < size2) {
+        if (arr1[i] <= arr2[j]) {
+            merged[k++] = arr1[i++];
+        } else {
+            merged[k++] = arr2[j++];
+        }
+    }
+while (i < size1) {
+        merged[k++] = arr1[i++];
+    }
+ while (j < size2) {
+        merged[k++] = arr2[j++];
+    }
+// Find the median
+    if (merged_size % 2 == 1) {
+        return merged[merged_size / 2]; // Odd number of elements
+    } else {
+        int mid1 = merged_size / 2;
+        int mid2 = mid1 - 1;
+        return (merged[mid1] + merged[mid2]) / 2.0; // Even number of elements
+    }
+}
+
+//
+
 
 
 int main() {
